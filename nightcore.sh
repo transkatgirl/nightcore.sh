4d1475e10c2aa070cdb4ae0ebbae6aa96337414f
Kat <10361870+katattakd@users.noreply.github.com>
2020-08-10 09:13:53 +0000

    Improved audio visualizer, minor tweaks

--- nightcore.sh
+++ nightcore.sh
@@ -6,7 +6,7 @@
 ##### Tunables:
 
 # Change the amount that waifu2x denoises the image (0-3). Setting this too high can result in loss of detail, especially in non-anime images.
-export waifu2x_denoise_amount=3
+export waifu2x_denoise_amount=2
 
 # Change the colors used in the visualizer.
 export visualizer_colors="0x111111|0x111111"
@@ -14,27 +14,14 @@ export visualizer_colors="0x111111|0x111111"
 # Change the number of bars shown on the visualizer. 120 is a decent default.
 export visualizer_bars=120
 
-# Change the FFT size used for the visualizer. Higher sizes are more accurate, but are less responsive. Lower sizes are more response, but less accurate. This number must be a power of 2.
-export visualizer_fft_size=4096
-
-# Change the highest frequency shown on the visualizer (up to 24,000hz). 4,000hz is a decent default.
-export visualizer_max_frequency=4000
-
-# Change how the visualizer displays loudness. Possible options are lin, sqrt, cbrt, and log.
-export visualizer_loudness_curve="log"
-
-# Change the number of loudness values that are cropped out of the visualizer (1080 total, log scale). 200 is a decent default, but it may need to be changed depending on the loudness curve used.
-export visualizer_crop_amount=200
-
 # Change the opacity of the visualizer (from 0 to 1).
-export visualizer_opacity=0.8
+export visualizer_opacity=0.7
 
 # Change the x265 video compression preset used. Available options are ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, and veryslow. Slower presets will result in more efficient compression.
 export x265_encoder_preset="slow"
 
 ##### Start of code
 
-visualizer_total_bars=$(awk 'BEGIN{ print int('$visualizer_bars'/(log('$visualizer_max_frequency')/log(24000))) }')
 ffloglevelstr="-loglevel error -y"
 sxloglevelstr="-V1"
 w2loglevelstr="-v 0"
@@ -52,11 +39,11 @@ if [[ ! -f "speed.txt" ]]; then
 	exit
 fi
 
-# Remove metadata and speed up audio.
+# Remove metadata, fix clipping, and speed up audio.
 for i in "${afiletypes[@]}"; do
 	if [[ -f "$i" ]]; then
 		echo "Processing audio..."
-		ffmpeg $ffloglevelstr -i $i -vn -map_metadata -1 -f sox - | sox $sxloglevelstr -p /tmp/audio.wav --guard --multi-threaded --buffer 1000000 speed "$(cat speed.txt)" rate -v -I 48k gain -n
+		ffmpeg $ffloglevelstr -i $i -vn -map_metadata -1 -af "volume=-15dB,adeclip=m=s,afade=t=in:ss=0:d=0.5:curve=squ,silenceremove=start_threshold=-105dB:start_mode=all:stop_periods=-1" -f sox - | sox $sxloglevelstr -p /tmp/audio.wav --guard --multi-threaded --buffer 1000000 speed "$(cat speed.txt)" rate -v -I 48k gain -n
 		break
 	fi
 done
@@ -66,12 +53,13 @@ if [[ ! -f "/tmp/audio.wav" ]]; then
 	exit
 fi
 
-# Remove metadata and AI upscale image.
+# Remove metadata, crop image to 16:9, and AI upscale image.
 for i in "${vfiletypes[@]}"; do
 	if [[ -f "$i" ]]; then
 		echo "Processing image..."
-		width=$(ffprobe -v 0 -of csv=p=0 -select_streams v:0 -show_entries stream=width "$i")
-		height=$(ffprobe -v 0 -of csv=p=0 -select_streams v:0 -show_entries stream=height "$i")
+		ffmpeg $ffloglevelstr -i $i -an -vframes 1 -map_metadata -1 -vf "crop=iw:round((iw/16)*9)" /tmp/input.ppm
+		width=$(ffprobe -v 0 -of csv=p=0 -select_streams v:0 -show_entries stream=width /tmp/input.ppm)
+		height=$(ffprobe -v 0 -of csv=p=0 -select_streams v:0 -show_entries stream=height /tmp/input.ppm)
 		if [ "$width" -ge "4000" ] && [ "$height" -ge "2320" ]; then
 			w2x_scale=1
 		elif [ "$width" -ge "2000" ] && [ "$height" -ge "1160" ]; then
@@ -90,9 +78,9 @@ for i in "${vfiletypes[@]}"; do
 			w2x_scale=8
 		else
 			echo "Input image is too small!"
+			rm /tmp/input.ppm
 			exit
 		fi
-		ffmpeg $ffloglevelstr -i $i -an -vframes 1 -map_metadata -1 /tmp/input.ppm
 		waifu2x-converter-cpp $w2loglevelstr -m noise-scale --scale-ratio $w2x_scale --noise-level $waifu2x_denoise_amount -i /tmp/input.ppm -o /tmp/background.ppm
 		rm /tmp/input.ppm
 		break
@@ -123,13 +111,13 @@ loudnorm_i=$(echo "$loudnorm" | grep "Input Integrated" | awk '{ print $3 }')
 loudnorm_tp=$(echo "$loudnorm" | grep "Input True Peak" | awk '{ print $4 }')
 loudnorm_lra=$(echo "$loudnorm" | grep "Input LRA" | awk '{ print $3 }')
 loudnorm_thresh=$(echo "$loudnorm" | grep "Input Threshold" | awk '{ print $3 }')
-filtergraph="[0:a]afade=t=in:ss=0:d=0.5:curve=squ,silenceremove=start_threshold=-115dB:start_mode=all:stop_periods=-1,loudnorm=linear=true:measured_i=$loudnorm_i:measured_lra=$loudnorm_lra:measured_tp=$loudnorm_tp:measured_thresh=$loudnorm_thresh,adeclip=m=s,showfreqs=s=$(($visualizer_total_bars))x1080:mode=bar:ascale=$visualizer_loudness_curve:fscale=log:colors=$visualizer_colors:win_size=$visualizer_fft_size:win_func=bharris,crop=$visualizer_bars:1080:0:0,scale=3840x1080:sws_flags=neighbor,setsar=0,format=rgba,colorchannelmixer=aa=$visualizer_opacity[visualizer];
-[1:v]scale=4000x2320:force_original_aspect_ratio=increase:sws_flags=lanczos+accurate_rnd+full_chroma_int+full_chroma_inp+bitexact,crop=4000:2320,loop=loop=-1:size=1,crop=3840:2160:$filterx:$filtery[background];
-[background][visualizer]overlay=shortest=1:x=0:y=$((1080+$visualizer_crop_amount)):format=rgb"
+filtergraph="[0:a]loudnorm=linear=true:measured_i=$loudnorm_i:measured_lra=$loudnorm_lra:measured_tp=$loudnorm_tp:measured_thresh=$loudnorm_thresh,showcqt=s=${visualizer_bars}x1080:r=60:axis_h=0:sono_h=0:bar_v=36dB*a_weighting(f):bar_g=7:count=30:cscheme=0.0001|0.0001|0.0001|0.0001|0.0001|0.0001,scale=3840x1080:sws_flags=neighbor,setsar=0,format=rgba,colorkey=black:0.01:0,colorchannelmixer=aa=$visualizer_opacity[visualizer];
+[1:v]scale=4000x2320:force_original_aspect_ratio=increase:sws_flags=lanczos+accurate_rnd+full_chroma_int+full_chroma_inp+bitexact,crop=3840:2160:$filterx:$filtery[background];
+[background][visualizer]overlay=shortest=1:x=0:y=1080:format=rgb"
 
 # Render video with generated filtergraph
 echo "Rendering video..."
-ffmpeg $ffloglevelstr -stats -i /tmp/audio.wav -i /tmp/background.ppm -c:v libx265 -r 60 -filter_complex "$filtergraph" -x265-params lossless=1 -preset "$x265_encoder_preset" -c:a flac -compression_level 12 -exact_rice_parameters 1 output.mkv
+ffmpeg $ffloglevelstr -stats -i /tmp/audio.wav -loop 1 -i /tmp/background.ppm -c:v libx265 -r 60 -filter_complex "$filtergraph" -x265-params lossless=1 -preset "$x265_encoder_preset" -c:a flac -compression_level 12 -exact_rice_parameters 1 output.mkv
 rm /tmp/background.ppm
 rm /tmp/audio.wav
 
