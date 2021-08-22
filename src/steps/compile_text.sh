a932a433fa082d63ddd94ab41ebb8aec1f0d9baf
Kat <10361870+katattakd@users.noreply.github.com>
2021-08-22 16:09:27 -0700

    Fixed title escaping

--- src/steps/compile_text.sh
+++ src/steps/compile_text.sh
@@ -10,10 +10,10 @@ font_align="$video_font_align"
 calculate_sizes
 
 if [ "$video_font_align" == "right" ]; then
-	title="`cat "$tmpdir/title.txt"` [`cat "$tmpdir/speed.txt"`x speed]"
+	echo "`cat "$tmpdir/title.txt"` [`cat "$tmpdir/speed.txt"`x speed]" > "$tmpdir/video_title.txt"
 else
-	title="[`cat "$tmpdir/speed.txt"`x speed] `cat "$tmpdir/title.txt"`"
+	echo "[`cat "$tmpdir/speed.txt"`x speed] `cat "$tmpdir/title.txt"`" > "$tmpdir/video_title.txt"
 fi
 
-echo "$drawtext:fontsize=$font_size:text='$title':x=$font_x:y=$(($padding*3)),\
+echo "$drawtext:fontsize=$font_size:textfile='$tmpdir/video_title.txt':x=$font_x:y=$(($padding*3)),\
 $drawtext:fontsize=$info_font_size:textfile='$tmpdir/info.txt':x=$font_x:y=$((($padding*6)+$font_size))" > "$1"
