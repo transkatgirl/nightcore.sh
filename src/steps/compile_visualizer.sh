a8cb9e2bb7f854f01e68e31aa966506a2256c35e
Kat <10361870+katattakd@users.noreply.github.com>
2021-09-04 18:01:03 -0700

    Bug fixes

--- src/steps/compile_visualizer.sh
+++ src/steps/compile_visualizer.sh
@@ -10,6 +10,8 @@ height=$(( $output_height / 2 ))
 basefreq=`echo "$1" | awk '{ print $1 * 20 }'`
 endfreq=`echo "$1" | awk '{ print $1 * 12500 }'`
 
+lufs=`echo $loudness_target_lufs | abs`
+
 # based on visualizer.sh (https://gist.github.com/katattakd/cc81d24f3b05db19a02373a085f207f7)
 visualizer="showcqt=s=110x$height:r=$output_framerate:\
 axis_h=0:sono_h=0:\
@@ -20,7 +22,7 @@ count=`echo 3 $height | awk '{ print ($1 * $2)/120 }'`:\
 basefreq=$basefreq:endfreq=$endfreq:\
 cscheme=0.2|0.2|0.2|0.2|0.2|0.2"
 
-echo "volume=$((${loudness_target_lufs}-10))dB,$visualizer,\
+echo "volume=$(($lufs-10))dB,$visualizer,\
 setsar=0,format=rgba,\
 colorkey=black:0.01:0,\
 lut=c0=$vr:c1=$vg:c2=$vb:c3=if(val\,194\,0),\
